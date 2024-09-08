3a2827d00648383493ed976578f14bc13ab4bc25
lulav <iftahnaf@gmail.com>
2024-09-08 14:56:34 +0300

    fix: using jazzy so cv_bridge.hpp

--- picamera_ros2/include/picamera_ros2/picamera_pub.hpp
+++ picamera_ros2/include/picamera_ros2/picamera_pub.hpp
@@ -7,7 +7,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 // #include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 #include <picamera_param/picamera_param.hpp>
 
